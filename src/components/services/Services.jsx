72d3e64ac7ff8576522db2eefbb76ef285754031
Saidumarov <saidumarovjafarxon@gmail06.com>
2023-08-22 07:36:50 +0500

    servis

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -5,9 +5,9 @@ const Services = () => {
   return (
     <div id="services">
       <div className="services">
-        <div className="services-me reveal">My Services</div>
+        <div className="services-me reveal"> <span className="opasity"> My Services</span> <span className="myser">My Services</span></div>
         <div className="services-itme reveal ">
-          <p style={{ textAlign: "center" }}>
+          <p style={{ textAlign: "center" , fontSize:"18px"}} >
             Mening xizmatlarim Web Design, Web Developer ,Youtuber
           </p>
           <div className="services-itmes">
